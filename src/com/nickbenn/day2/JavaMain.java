b5efa841e52fc68f587606fccd5fcc49b4590541
Nick Bennett <nick@nickbenn.com>
2019-12-01 14:27:43 -0700

    Prep for day 2.

--- src/com/nickbenn/day2/JavaMain.java
+++ src/com/nickbenn/day2/JavaMain.java
@@ -2,18 +2,15 @@ package com.nickbenn.day2;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
-import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.function.IntUnaryOperator;
-import java.util.stream.Stream;
 
-public class Main {
+public class JavaMain {
 
   private static final String INPUT_FILE = "day2/input.txt";
   private static final String OUTPUT_FORMAT = "Part %d: %,d%n";
 
   public static void main(String[] args) throws URISyntaxException, IOException {
-    Path path = Path.of(Main.class.getClassLoader().getResource(INPUT_FILE).toURI());
+    Path path = Path.of(JavaMain.class.getClassLoader().getResource(INPUT_FILE).toURI());
     System.out.printf(OUTPUT_FORMAT, 1, compute(path));
     System.out.printf(OUTPUT_FORMAT, 2, compute(path));
   }
