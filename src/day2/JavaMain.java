8527274bbf93b25d90ee1923abd109474d97a0fd
Nick Bennett <nick@nickbenn.com>
2019-12-02 01:21:26 -0700

    Fix copyright statement.

--- src/day2/JavaMain.java
+++ src/day2/JavaMain.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2019 Nicholas Bennett & Deep Dive Coding/CNM Ingenuity
+ *  Copyright 2019 Nicholas Bennett
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
