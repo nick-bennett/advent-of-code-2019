fa1cc6365cf1f18be6e8884086fd791b135ac313
Nick Bennett <nick@nickbenn.com>
2019-12-04 23:22:28 -0700

    Java solution for day 5.

--- src/day5/JavaMain.java
+++ src/day5/JavaMain.java
@@ -19,6 +19,9 @@ import java.io.IOException;
 import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
@@ -28,23 +31,125 @@ public class JavaMain {
 
   private static final String INPUT_FILE = "input.txt";
   private static final Pattern DELIMITER = Pattern.compile("\\s*,\\s*");
+  private static final String OUTPUT_FORMAT = "Part %d: For input = %d, diagnostic output = %d.%n";
 
   public static void main(String[] args) throws URISyntaxException, IOException {
     Path path = Path.of(JavaMain.class.getResource(INPUT_FILE).toURI());
-    List<List<String>> data = parse(path);
-    System.out.println(process(data));
+    int[] instructions = parse(path);
+    System.out.printf(
+        OUTPUT_FORMAT, 1, 1, process(Arrays.copyOf(instructions, instructions.length), 1));
+    System.out.printf(
+        OUTPUT_FORMAT, 2, 5, process(Arrays.copyOf(instructions, instructions.length), 5));
   }
 
-  public static List<List<String>> parse(Path path) throws IOException {
+  public static int[] parse(Path path) throws IOException {
     try (Stream<String> stream = Files.lines(path)) {
       return stream
-          .map((line) -> DELIMITER.splitAsStream(line).collect(Collectors.toList()))
-          .collect(Collectors.toList());
+          .flatMap(DELIMITER::splitAsStream)
+          .mapToInt(Integer::parseInt)
+          .toArray();
     }
   }
 
-  private static int process(List<List<String>> data) {
-    return 0;
+  private static int process(int[] instructions, int input) {
+    int result = 0;
+    int ip = 0;
+    while (true) {
+      int operation = instructions[ip++];
+      int opcode = operation % 100;
+      operation = operation / 100;
+      int[] modes;
+      int[] operands;
+      switch (opcode) {
+        case 1:
+          operands = consume(instructions, ip, 3);
+          modes = modes(operation, 2);
+          preprocess(operands, modes,instructions);
+          instructions[operands[2]] = operands[0] + operands[1];
+          ip += 3;
+          break;
+        case 2:
+          operands = consume(instructions, ip, 3);
+          modes = modes(operation, 2);
+          preprocess(operands, modes,instructions);
+          instructions[operands[2]] = operands[0] * operands[1];
+          ip += 3;
+          break;
+        case 3:
+          operands = consume(instructions, ip, 1);
+          instructions[operands[0]] = input;
+          ip += 1;
+          break;
+        case 4:
+          operands = consume(instructions, ip, 1);
+          modes = modes(operation, 1);
+          preprocess(operands, modes,instructions);
+          if (result != 0) {
+            throw new IllegalArgumentException();
+          } else {
+            result = operands[0];
+          }
+          ip += 1;
+          break;
+        case 5:
+          operands = consume(instructions, ip, 2);
+          modes = modes(operation, 2);
+          preprocess(operands, modes,instructions);
+          if (operands[0] != 0) {
+            ip = operands[1];
+          } else {
+            ip += 2;
+          }
+          break;
+        case 6:
+          operands = consume(instructions, ip, 2);
+          modes = modes(operation, 2);
+          preprocess(operands, modes,instructions);
+          if (operands[0] == 0) {
+            ip = operands[1];
+          } else {
+            ip += 2;
+          }
+          break;
+        case 7:
+          operands = consume(instructions, ip, 3);
+          modes = modes(operation, 2);
+          preprocess(operands, modes,instructions);
+          instructions[operands[2]] = (operands[0] < operands[1]) ? 1 : 0;
+          ip += 3;
+          break;
+        case 8:
+          operands = consume(instructions, ip, 3);
+          modes = modes(operation, 2);
+          preprocess(operands, modes,instructions);
+          instructions[operands[2]] = (operands[0] == operands[1]) ? 1 : 0;
+          ip += 3;
+          break;
+        case 99:
+          return result;
+      }
+    }
+  }
+
+  private static int[] modes(int operation, int length) {
+    int[] modes = new int[length];
+    for (int i = 0; i < modes.length; i++) {
+      modes[i] = operation % 10;
+      operation /= 10;
+    }
+    return modes;
+  }
+
+  private static int[] consume(int[] instructions, int ip, int length) {
+    return Arrays.copyOfRange(instructions, ip, ip + length);
+  }
+
+  private static void preprocess(int[] operands, int[] modes, int[] instructions) {
+    for (int i = 0; i < modes.length; i++) {
+      if (modes[i] == 0) {
+        operands[i] = instructions[operands[i]];
+      }
+    }
   }
 
 }
